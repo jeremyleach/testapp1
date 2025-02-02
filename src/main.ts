026fbe08b380940c7bde74816af1a52e5e97bb39
Jeremy Leach <jeremyleach@Jeremys-Laptop.local>
2025-02-02 17:21:48 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,170 @@
+import { Application, Graphics, Text } from 'pixi.js'
+import shapesData from './shapes.json' 
+
+// Define the structure of a single vertex
+interface Vertex {
+  x: number
+  y: number
+  radius?: number
+}
+
+// Define the structure of the shapes object
+interface ShapesJson {
+  [shapeName: string]: Vertex[]
+}
+
+type AllowedShapes = keyof typeof shapesData
+const shapes: ShapesJson = shapesData
+const shapeNames: AllowedShapes[] = Object.keys(shapes) as AllowedShapes[]
+
+// colurs for background and stroke of shape
+const colours = [
+  "0xFF0000", // red
+  "0x00FF00", // green
+  "0xFFFF00", // yellow
+  "0x0000FF", // blue
+]
+
+// stroke widths to cycle through
+const widths = [
+  2,4,6,8
+]
+
+// Get vertices for a specific shape, rotated around centre.
+const rotatePoints = (points: Vertex[], angle: number) => {
+
+  const cosA = Math.cos(angle);
+  const sinA = Math.sin(angle);
+
+  const vertices: Vertex[] = points.map((vertex: Vertex) => ({
+    x: (cosA * vertex.x - sinA * vertex.y),
+    y: (sinA * vertex.x + cosA * vertex.y),
+    radius: vertex.radius
+  }))
+  return vertices
+}
+
+// scale and translate the points of a shape
+const scaleAndShiftPoints = (points: Vertex[], scale: number, deltaX: number, deltaY: number) => {
+  return points.map(point => ({
+    x: point.x * scale + deltaX,
+    y: point.y * scale + deltaY,
+    radius: point.radius ? point.radius * scale : undefined
+  }))
+}
+
+// Create the PixiJS application
+const app = new Application()
+const defaultSize = 100.0;
+
+(async () => {
+  await app.init({
+      antialias: true,
+      resizeTo: window,
+      backgroundColor: 0xeeeeee    
+  })
+
+  // add canvas to page
+  document.body.appendChild(app.canvas)
+
+  // Create shape
+  let shape = new Graphics()
+  let shapeText = new Text({
+
+  })
+  
+  // Set rotation parameters
+  let currentRotation = 0
+  let zoom = 1.0
+  let shapeIndex = 0
+  let colourIndex = 0
+  let bgIndex = 1
+  let widthIndex = 0
+  
+  const renderScene = () => {
+
+    shape.clear();
+    
+    // Calculate new points and draw
+    const rotatedPoints = rotatePoints(shapes[shapeNames[shapeIndex]], currentRotation)
+    const scaledPoints = scaleAndShiftPoints(rotatedPoints, defaultSize * zoom, app.screen.width/2, app.screen.height/2)
+
+    if(shapeNames[shapeIndex] === "rounded rectangle") {
+
+      shape.moveTo(scaledPoints[0].x, scaledPoints[0].y)      
+      for(let i=1; i<=scaledPoints.length; i++) {
+        if(typeof scaledPoints[i % scaledPoints.length].radius !== 'undefined') {
+          shape.arcTo(
+            scaledPoints[i % scaledPoints.length].x, 
+            scaledPoints[i % scaledPoints.length].y,
+            scaledPoints[(i+1) % scaledPoints.length].x, 
+            scaledPoints[(i+1) % scaledPoints.length].y,
+            scaledPoints[(i) % scaledPoints.length].radius ||= 0
+          )
+        } else {
+          shape.lineTo(scaledPoints[i % scaledPoints.length].x, scaledPoints[i % scaledPoints.length].y)
+        }
+      }
+    } else {
+      shape
+        .poly(scaledPoints)
+    }
+    shape.fill(colours[bgIndex])
+    .stroke({ color: colours[colourIndex], width: widths[widthIndex] })
+
+    // set the text to new shape name
+    shapeText.text = shapeNames[shapeIndex]
+    shapeText.x = app.screen.width/2 - shapeText.width/2; // show text in x-axis centre
+    shapeText.y = 10; // bit of top margin
+  }
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    const key = event.key;
+
+    switch(key) {
+      case 'ArrowUp':
+        if(zoom < 4) zoom *= 1.2
+        break
+      case 'ArrowDown':
+        if(zoom > 0.25) zoom /= 1.2
+        break
+      case 'ArrowLeft':
+        currentRotation -= Math.PI / 20
+        break
+      case 'ArrowRight':
+        currentRotation += Math.PI / 20
+        break
+      case 's':
+        shapeIndex++
+        shapeIndex %= shapeNames.length
+        break
+      case 'e':
+        colourIndex++
+        colourIndex %= colours.length
+        break
+      case 'b':
+        bgIndex++
+        bgIndex %= colours.length
+        break
+      case 't':
+        widthIndex++
+        widthIndex %= widths.length
+        break;
+      default:
+        break
+    }
+    console.log(key)
+    renderScene()
+  }
+  
+  app.renderer.on('resize', () => {
+    renderScene()
+  })
+
+  document.addEventListener('keydown', onKeyDown)
+
+  app.stage.addChild(shape)
+  app.stage.addChild(shapeText)
+  renderScene()
+
+})()
